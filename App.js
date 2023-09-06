4aeafc12c1a293f9335af1b127edeaabf465c712
Malinda Wimalasena <malinda@geveo.com>
2023-09-06 22:52:30 +0530

    Removed deprecated package

--- App.js
+++ App.js
@@ -1,109 +1,91 @@
-//web 176964422252-1r6fd1n8ii3kjcl91egh1kpc6aqsdbuu.apps.googleusercontent.com
-//android 176964422252-m21vkiraioam8stscqt8qp64rk374qs6.apps.googleusercontent.com
-//ios 176964422252-coav2tace8hjd1fo0skjtn9597qe7688.apps.googleusercontent.com 
-
-//MS
-// --- Geveo AD
-//   clientId: '42d95333-1920-4be8-a244-afa38c7d4463',
-//   tenant:  '2dc5556c-805e-4168-9b18-91a81ce717f9', // your app tenant ID
-
-// --- My person AD
-//  clientId: '70170c33-4a8c-4690-8618-4eedc21dc9e8',
-//  tenant: 'e48c12e9-6f51-4aa2-ab71-dcdc690ad071', // your app tenant ID
-
 import * as React from 'react'
 import { Button, StyleSheet, Text, View } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
-import * as Google from 'expo-auth-session/providers/google'
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import AzureAuth from 'react-native-azure-auth';
+import {
+  GoogleSignin,
+} from '@react-native-google-signin/google-signin';
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
   const [accessToken, setAccessToken] = React.useState(null);
   const [userInfo, setUserInfo] = React.useState(null);
-  const [request, response, promptAsync] =  Google.useAuthRequest({
-    androidClientId: "176964422252-m21vkiraioam8stscqt8qp64rk374qs6.apps.googleusercontent.com",
-    iosClientId: "176964422252-coav2tace8hjd1fo0skjtn9597qe7688.apps.googleusercontent.com",
-    webClientId: "176964422252-1r6fd1n8ii3kjcl91egh1kpc6aqsdbuu.apps.googleusercontent.com",
-  });
+  const [ssoProvider, setSSOProvider] = React.useState(null);
 
   const azureAuth = new AzureAuth({
     clientId: '70170c33-4a8c-4690-8618-4eedc21dc9e8',
   });
 
-  React.useEffect(()=>{
-    handleSignInWithGoogle();
-  }, [response]);
+  const MICROSFT_PROVIDER = "Microsoft";
+  const GOOGLE_PROVIDER = "Google";
 
-  async function handleSignInWithGoogle(){
-    const user = await AsyncStorage.getItem("@user");
-    if (!user){
-      if(response?.type === "success"){
-        setAccessToken(response.authentication.accessToken);
-        await getUserInfo(response.authentication.accessToken);
-      }
-    } 
-    else {
-      setUserInfo(JSON.parse(user));
-    }
-  }
-
-  const getUserInfo = async (token) => {
-    if (!token) return;
+  const _onLoginMircrosoft = async () => {
     try {
-      const response = await fetch(
-        "https://www.googleapis.com/userinfo/v2/me",
-        {
-          headers: { Authorization: `Bearer ${token}`},
-        }
-      )
-
-      const user = await response.json();
-      await AsyncStorage.setItem("@user", JSON.stringify(user));
-      setUserInfo(user);
-    } catch (error) {
-      
-    }
-  }
-
-  const _onLogin = async () => {
-    try {
+      setSSOProvider(MICROSFT_PROVIDER);
       let tokens = await azureAuth.webAuth.authorize({scope: 'openid profile User.Read' })
       setAccessToken(tokens.accessToken);
       let info = await azureAuth.auth.msGraphRequest({token: tokens.accessToken, path: '/me'})
       setUserInfo({ name: info.displayName, userId: tokens.userId })
-    } catch (error) {
-      console.log(error)
+    } catch (e) {
+      console.log("ERROR IS: " + JSON.stringify(e));
     }
+  }
+
+  const _onLoginGoogle = async () => {
+    setSSOProvider(GOOGLE_PROVIDER);
+    GoogleSignin.configure({
+      offlineAccess: true,
+      androidClientId: "176964422252-m21vkiraioam8stscqt8qp64rk374qs6.apps.googleusercontent.com",
+      iosClientId: "176964422252-coav2tace8hjd1fo0skjtn9597qe7688.apps.googleusercontent.com",
+      webClientId: "176964422252-1r6fd1n8ii3kjcl91egh1kpc6aqsdbuu.apps.googleusercontent.com",
+    });
+    GoogleSignin.hasPlayServices().then((hasPlayService) => {
+            if (hasPlayService) {
+                GoogleSignin.signIn().then((info) => {
+                  setUserInfo({ name: info.user.name, userId: info.user.name })
+                  setAccessToken(info.idToken);
+                  console.log(JSON.stringify(userInfo, null, 2))
+                }).catch((e) => {
+                console.log("ERROR IS: " + JSON.stringify(e));
+                })
+            }
+    }).catch((e) => {
+        console.log("ERROR IS: " + JSON.stringify(e));
+    })
   }
 
   const _onLogout = async () => {
     setUserInfo(null);
     setAccessToken(null);
-    await AsyncStorage.removeItem("@user");
+    if(ssoProvider === MICROSFT_PROVIDER){
+      await azureAuth.webAuth.clearSession();
+    }
+    else {
+      await GoogleSignin.signOut();
+    }
+    setSSOProvider(null);
   }
-
+ 
 return (
   <View style={styles.container}>
     <View>
-      <Text style={styles.header}>Azure Auth - Login</Text>
-      <Text style={styles.text}>Hello {userInfo ? userInfo.name : 'USER'} !</Text>
-      <Text style={styles.text}>
+      <Text style={styles.header}>{ssoProvider ? (ssoProvider === MICROSFT_PROVIDER ? MICROSFT_PROVIDER : GOOGLE_PROVIDER) : ''} Auth - Login</Text>
+      <Text style={styles.text}>Hello {userInfo ? userInfo.name : ''} !!!</Text>
+      <Text style={styles.loginText}>
         You are {accessToken  ? '' : 'not '}logged in.
       </Text>
-    </View>  
+    </View> 
     <View style={styles.buttons}> 
       {!accessToken && (<Button
         style={styles.button}
-        onPress={_onLogin}
-        title={'Microsoft Log In'}
+        onPress={_onLoginMircrosoft}
+        title={'Microsoft - Login'}
       />)}
       {!accessToken && (<Button
         style={styles.button}
-        onPress={ () => promptAsync()}
-        title={'Google Log In'}
+        onPress={ () => _onLoginGoogle()}
+        title={'Google - Login'}
       />)}
       {accessToken && (<Button 
         style={styles.button}
@@ -111,7 +93,7 @@ return (
         title={'Log Out'}
       />)}
     </View>
-</View>
+  </View>
 )}
 
 const styles = StyleSheet.create({
@@ -119,15 +101,24 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'stretch',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: 'pink'
   },
   header: {
-    fontSize: 20,
+    fontSize: 30,
     textAlign: 'center',
-    margin: 10
+    margin: 10,
+    backgroundColor: 'black',
+    color: 'white'
   },
   text: {
-    textAlign: 'center'
+    textAlign: 'center',
+    fontSize: 25,
+    backgroundColor: '#ecedce'
+  },
+  loginText: {
+    textAlign: 'center',
+    fontSize: 20,
+    backgroundColor: '#ff7373'
   },
   buttons: {
     flexDirection: 'row',
